1414d98a51323cb0b921cb2382b2a1fc76f059cd
Ali Khalili <ali.khll.1994@gmail.com>
2016-11-08 15:13:33 +0330

    domain models + context added

---
+++ src/PgCore/DomainModelPostgreSqlContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace PgCore
+{
+    //dotnet ef migrations add testPG
+    //dotnet ef database update
+    public class DomainModelPostgreSqlContext : DbContext
+    {
+        public DomainModelPostgreSqlContext(DbContextOptions<DomainModelPostgreSqlContext> options) : base(options)
+        {
+        }
+
+        public DbSet<DataEventRecord> DataEventRecords { get; set; }
+
+        public DbSet<SourceInfo> SourceInfos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<DataEventRecord>().HasKey(m => m.DataEventRecordId);
+            builder.Entity<SourceInfo>().HasKey(m => m.SourceInfoId);
+
+            // shadow properties
+            builder.Entity<DataEventRecord>().Property<DateTime>("UpdatedTimestamp");
+            builder.Entity<SourceInfo>().Property<DateTime>("UpdatedTimestamp");
+
+            base.OnModelCreating(builder);
+        }
+
+        public override int SaveChanges()
+        {
+            ChangeTracker.DetectChanges();
+
+            updateUpdatedProperty<SourceInfo>();
+            updateUpdatedProperty<DataEventRecord>();
+
+            return base.SaveChanges();
+        }
+
+        private void updateUpdatedProperty<T>() where T : class
+        {
+            var modifiedSourceInfo =
+                ChangeTracker.Entries<T>()
+                    .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+            foreach (var entry in modifiedSourceInfo)
+            {
+                entry.Property("UpdatedTimestamp").CurrentValue = DateTime.UtcNow;
+            }
+        }
+    }
+
+    public class DataEventRecord
+    {
+        public long DataEventRecordId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Timestamp { get; set; }
+        public SourceInfo SourceInfo { get; set; }
+        public int SourceInfoId { get; set; }
+    }
+
+    public class SourceInfo
+    {
+        public long SourceInfoId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Timestamp { get; set; }
+        public List<DataEventRecord> DataEventRecords { get; set; }
+    }
+}
