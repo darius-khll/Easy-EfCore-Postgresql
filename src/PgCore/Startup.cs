f12c709fd55e8d03467d1def565bdbf7e3551ea9
Ali Khalili <ali.khll.1994@gmail.com>
2016-11-08 15:14:22 +0330

    startup configured

---
+++ src/PgCore/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace PgCore
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var sqlConnectionString = "User ID=postgres;Password=123;Host=localhost;Port=5433;Database=Db1;Pooling=true;";
+
+            services.AddDbContext<DomainModelPostgreSqlContext>(options =>
+                options.UseNpgsql(
+                    sqlConnectionString,
+                    b => b.MigrationsAssembly("PgCore")
+                )
+            );
+
+            services.AddScoped<IDataAccessProvider, PgCore.DataAccessPostgreSqlProvider>();
+
+            services.AddMvc();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvcWithDefaultRoute();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
