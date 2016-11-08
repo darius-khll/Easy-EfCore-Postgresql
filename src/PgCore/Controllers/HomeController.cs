ef6d8664df6645cac01faae28c3c3390756f9515
Ali Khalili <ali.khll.1994@gmail.com>
2016-11-08 15:14:06 +0330

    home controller added to project

---
+++ src/PgCore/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PgCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IDataAccessProvider _db;
+        public HomeController(IDataAccessProvider db)
+        {
+            _db = db;
+        }
+
+        public string Index()
+        {
+            var data1 = _db.GetDataEventRecords();
+            var data3 = _db.GetSourceInfos(true);
+            return "wow...";
+        }
+    }
+}
