313b5bd814364d6bf0a56d8c81df6ddf04820d2d
Ali Khalili <ali.khll.1994@gmail.com>
2016-11-08 15:15:02 +0330

    IDataAccess provider added

---
+++ src/PgCore/IDataAccessProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace PgCore
+{
+    public interface IDataAccessProvider
+    {
+        void AddDataEventRecord(DataEventRecord dataEventRecord);
+        void UpdateDataEventRecord(long dataEventRecordId, DataEventRecord dataEventRecord);
+        void DeleteDataEventRecord(long dataEventRecordId);
+        DataEventRecord GetDataEventRecord(long dataEventRecordId);
+        List<DataEventRecord> GetDataEventRecords();
+        List<SourceInfo> GetSourceInfos(bool withChildren);
+    }
+}
